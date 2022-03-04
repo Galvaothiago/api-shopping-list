83865650f6848ba9fdd1abc42a5e510a2fae641c
Thiago Galvao <thiagosilva128@gmail.com>
2022-03-03 23:58:46 -0300

    refactory: password validation

--- src/main/java/com/api/shopping/list/security/services/ValidatePolicyPassword.java
+++ src/main/java/com/api/shopping/list/security/services/ValidatePolicyPassword.java
@@ -27,7 +27,7 @@ public class ValidatePolicyPassword {
 	}
 	
 	public boolean validate() {
-		result = validator.validate(new PasswordData(password));
+		result = createValidationPolicy().validate(new PasswordData(password));
 		
 		if (result.isValid()) {
 			return true;		
@@ -36,39 +36,46 @@ public class ValidatePolicyPassword {
 	}
 	
 	public void brokenRulesMessagens() {
+		List<String> violationMessages = createValidationPolicy().getMessages(result);
+		
 		if(!this.validate()) {
-			for(String message : validator.getMessages(result)) {
+			for(String message : violationMessages) {
 				listRulesViolated.add(message);
 				System.out.println(message);
 			}
 		}
 	}
 
-	PasswordValidator validator = new PasswordValidator(
-			  // length between 8 and 16 characters
-			  new LengthRule(8, 16),
-
-			  // at least one upper-case character
-			  new CharacterRule(EnglishCharacterData.UpperCase, 1),
-
-			  // at least one lower-case character
-			  new CharacterRule(EnglishCharacterData.LowerCase, 1),
-
-			  // at least one digit character
-			  new CharacterRule(EnglishCharacterData.Digit, 1),
-
-			  // at least one symbol (special character)
-			  new CharacterRule(EnglishCharacterData.Special, 1),
-
-			  // define some illegal sequences that will fail when >= 5 chars long
-			  // alphabetical is of the form 'abcde', numerical is '34567', qwery is 'asdfg'
-			  // the false parameter indicates that wrapped sequences are allowed; e.g. 'xyzabc'
-			  new IllegalSequenceRule(EnglishSequenceData.Alphabetical, 3, false),
-			  new IllegalSequenceRule(EnglishSequenceData.Numerical, 3, false),
-			  new IllegalSequenceRule(EnglishSequenceData.USQwerty, 5, false),
-
-			  // no whitespace
-			  new WhitespaceRule());
+	private PasswordValidator createValidationPolicy() {
+		
+		var validator = new PasswordValidator(
+				//length between 8 and 16 characters
+				new LengthRule(8, 16),
+				
+				//at least one upper-case character
+				new CharacterRule(EnglishCharacterData.UpperCase, 1),
+				
+				//at least one lower-case character
+				new CharacterRule(EnglishCharacterData.LowerCase, 1),
+				
+				//at least one digit character
+				new CharacterRule(EnglishCharacterData.Digit, 1),
+				
+				//at least one symbol (special character)
+				new CharacterRule(EnglishCharacterData.Special, 1),
+				
+				//define some illegal sequences that will fail when >= 5 chars long
+				//alphabetical is of the form 'abcde', numerical is '34567', qwery is 'asdfg'
+				//the false parameter indicates that wrapped sequences are allowed; e.g. 'xyzabc'
+				new IllegalSequenceRule(EnglishSequenceData.Alphabetical, 3, false),
+				new IllegalSequenceRule(EnglishSequenceData.Numerical, 3, false),
+				new IllegalSequenceRule(EnglishSequenceData.USQwerty, 5, false),
+				
+				//no whitespace
+				new WhitespaceRule());
+		
+		return validator;
+	}
 
 
 	public List<String> getListRulesViolated() {
